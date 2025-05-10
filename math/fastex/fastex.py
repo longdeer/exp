425bb11fc677db3810a18e6982c50d727c16353e
lngd <lngdeer@gmail.com>
2025-05-10 11:39:59 +0300

    math/fastex/fastex.py

---
+++ math/fastex/fastex.py
@@ -0,0 +1,21 @@
+
+
+def recursive_fastex(self, b :int, e :int) -> int :
+
+    if not e : return 1
+    r = self.fastex(b,e >>1)
+    return (b*r*r if e &1 else r*r)
+
+
+def iterative_fastex(self, b :int, e :int) -> int :
+
+    r = 1
+
+    while   0 <e:
+        if  e &1 : r = (r*b)
+
+        b = (b*b)
+        e >>= 1
+
+    return r
+
