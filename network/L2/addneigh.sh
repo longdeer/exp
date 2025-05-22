aedab30e558b88bf51657c74fd0b19df5f3710cf
lngd <lngdeer@gmail.com>
2025-05-22 16:13:57 +0300

    network/L2/addneigh.sh fix (now manual)

--- network/L2/addneigh.sh
+++ network/L2/addneigh.sh
@@ -2,7 +2,6 @@
 ### manage ARP cache
 ### params:
 ### $1 - log file path string
-### $2 - device name string
 
 
 
@@ -12,7 +11,6 @@
 
 
 loggy=$1
-devname=$2
 
 
 
@@ -25,16 +23,17 @@ addneigh()
 # $1 - host ip4 string
 # $2 - host MAC string
 # $3 - host name string
+# $4 - interface name string
 {
 	echo "caching $3 at $2" >> $loggy
 
 	neighmac=$(echo $2 | sed -e s/-/:/g)
-	neightry=$(ip neigh add $1 lladdr $neighmac nud permanent dev $devname 2>&1)
+	neightry=$(ip neigh add $1 lladdr $neighmac nud permanent dev $4 2>&1)
 
 	if		[[ $neightry == "" ]]; then echo "adding done" >> $loggy
 	elif	[[ $neightry == "RTNETLINK answers: File exists"* ]]; then
 
-		neightry=$(ip neigh replace $1 lladdr $neighmac nud permanent dev $devname 2>&1)
+		neightry=$(ip neigh replace $1 lladdr $neighmac nud permanent dev $4 2>&1)
 
 		if		[[ $neightry == "" ]]; then echo "replacing done" >> $loggy
 		else	echo $neightry >> $loggy
