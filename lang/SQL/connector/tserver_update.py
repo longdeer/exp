7d19f5fbb468a55bef92146d642253efd21c29e4
lngd <lngdeer@gmail.com>
2025-05-28 14:12:10 +0300

    lang/SQL/connector/tserver_update prototype

---
+++ lang/SQL/connector/tserver_update.py
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+if	__name__ == "__main__":
+
+
+	import	mysql.connector
+	from	mysql.connector			import errorcode
+	from	pygwarts.irma.contrib	import LibraryContrib
+
+
+	try:
+
+		loggy = LibraryContrib(init_name="tserverdb")
+		connection = mysql.connector.connect(
+
+			user="vla",
+			password="vla::SQL",
+			host="192.168.162.65",
+			database="tserverdb",
+		)
+		tserver = connection.cursor()
+		loggy.info("Connection established")
+
+
+		tserver.execute("UPDATE navbag SET state = 0 WHERE word IN ('JAN', 'FEB', 'MAR', 'APR')")
+		tserver.execute("UPDATE navbag SET state = 0 WHERE word IN ('MAY', 'JUN', 'JUL', 'AUG')")
+
+
+		connection.commit()
+		tserver.close()
+
+
+	except	mysql.connector.Error as E: print(E)
+	else:	connection.close()
+
+
+
+
+
+
+
