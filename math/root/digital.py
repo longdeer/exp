8a43a938e93927e74c812f04f272f9758643c62f
lngd <lngdeer@gmail.com>
2025-06-13 10:15:02 +0300

    math/root/digital.py (mathematical + iterational)

--- math/root/digital.py
+++ math/root/digital.py
@@ -1,1 +1,5 @@
-def digital_root(n :int) -> int : return n %9 or n and 9
+def mathematical(n :int) -> int : return n %9 or n and 9
+def iterational(self, num :int) -> int :
+
+	while 1 <len(str(num)): num = sum(map(int,str(num)))
+	return num
