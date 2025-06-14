eb53eb9b1ff8f004f630cdebc3ddfcfcbf288adf
lngd <lngdeer@gmail.com>
2025-06-14 10:24:02 +0300

    math/root/digital.py (mathematical + iterative + recursive)

--- math/root/digital.py
+++ math/root/digital.py
@@ -1,5 +1,6 @@
 def mathematical(n :int) -> int : return n %9 or n and 9
-def iterational(self, num :int) -> int :
+def recursive(n :int) -> int : return n if len(str(n)) == 1 else addDigits(sum(map(int,str(n))))
+def iterative(n :int) -> int :
 
-	while 1 <len(str(num)): num = sum(map(int,str(num)))
-	return num
+	while 1 <len(str(n)): n = sum(map(int,str(n)))
+	return n
