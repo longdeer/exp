2251fa7aec57a1f3181c6edbc06777dfb3dac5fd
lngd <lngdeer@gmail.com>
2025-06-19 13:18:00 +0300

    math/root/digital.js (mathematical + recursive)

--- math/root/digital.js
+++ math/root/digital.js
@@ -1,3 +1,6 @@
 function mathematical(n) {
     return (r = n %9) ? r : n ? 9 : n
 }
+function recursive(n) {
+    return n <10 ? n : recursive(Array.prototype.reduce.call(n.toString(),(A,V) => Number(V) +A,0))
+}
