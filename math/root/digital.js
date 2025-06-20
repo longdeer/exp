777d084f58592ce5f8bed0e57837207fa573721f
lngd <lngdeer@gmail.com>
2025-06-20 09:37:07 +0300

    math/root/digital.js (mathematical + recursive + iterative)

--- math/root/digital.js
+++ math/root/digital.js
@@ -4,3 +4,18 @@ function mathematical(n) {
 function recursive(n) {
     return n <10 ? n : recursive(Array.prototype.reduce.call(n.toString(),(A,V) => Number(V) +A,0))
 }
+function iterative(n) {
+    var sum;
+
+    while(9 <n) {
+        sum = 0;
+
+        while(n) {
+
+            sum += n %10;
+            n = Math.floor(n /10);
+        }
+        n = sum;
+    }
+    return n;
+}
