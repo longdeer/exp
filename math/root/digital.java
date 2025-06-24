326a21558c57c69c81c7b91b79bee054602aa39d
lngd <lngdeer@gmail.com>
2025-06-24 12:48:33 +0300

    math/root/digital.java (mathematical + iterative)

--- math/root/digital.java
+++ math/root/digital.java
@@ -1,3 +1,20 @@
 public int mathematical(int n) {
 	return (n -1) %9 +1;
 }
+public int iterative(int n) {
+
+	int sum;
+
+	while(9 <n) {
+
+		sum = 0;
+
+		while(0 <n) {
+
+			sum += n %10;
+			n = (int) Math.floor(n /10);
+		}
+		n = sum;
+	}
+	return  n;
+}
