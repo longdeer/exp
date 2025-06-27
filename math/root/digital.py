e7ed193366cb537c3dea84b591b8d3b06427bbd9
lngd <lngdeer@gmail.com>
2025-06-27 17:34:27 +0300

    math/root/digital.py (mathematical + logical + iterative + recursive)

--- math/root/digital.py
+++ math/root/digital.py
@@ -1,4 +1,5 @@
-def mathematical(n :int) -> int : return n %9 or n and 9
+def mathematical(n: int) -> int : return (((n -1) %9) if n else -1) +1
+def logical(n :int) -> int : return n %9 or n and 9
 def recursive(n :int) -> int : return n if len(str(n)) == 1 else recursive(sum(map(int,str(n))))
 def iterative(n :int) -> int :
 
