8c814877d75f37a90de1ea57726b934ee8b419ca
lngd <lngdeer@gmail.com>
2025-07-10 11:19:53 +0300

    lang/JS/randomizer (prototype)

---
+++ lang/JS/randomizer.js
@@ -0,0 +1,9 @@
+
+
+var randomizer = {
+
+	get octet() { return Math.floor(Math.random() *256) },
+	get uint16() { return Math.floor(Math.random() *65536) },
+	get int16() { return Math.floor(Math.random() *65536) -32768 }
+}
+
