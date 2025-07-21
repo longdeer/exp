7d500f0b89fa49b847654abdced5751b29cb0890
lngd <lngdeer@gmail.com>
2025-07-21 13:06:37 +0300

    math/primes/sieve.c

---
+++ math/primes/sieve.c
@@ -0,0 +1,18 @@
+
+
+int* sift(int size)
+{
+	int* sieve = calloc(size +1,sizeof(int));
+	long long j;
+	long long i;
+
+	for(i = 2; i <= size; ++i) *(sieve +i) = 1;
+	for(i = 2; i <= size; ++i)
+
+		if(*(sieve +i))
+			for(j = i*i; j <= size; j += i)
+				*(sieve +j) = 0;
+
+	return	sieve;
+}
+
