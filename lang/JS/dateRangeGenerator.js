e41ff015479b110f726257e3204c2585cf97638f
lngd <lngdeer@gmail.com>
2025-07-27 16:49:50 +0300

    lang/JS/dateRangeGenerator

---
+++ lang/JS/dateRangeGenerator.js
@@ -0,0 +1,21 @@
+
+
+// LC2777
+// Given a start date start, an end date end, and a positive integer step,
+// return a generator object that yields dates in the range from start to end inclusive.
+// The value of step indicates the number of days between consecutive yielded values.
+// All yielded dates must be in the string format YYYY-MM-DD
+
+
+var dateRangeGenerator = function* (start, end, step) {
+
+	let current = new Date(start);
+	let finish = new Date(end);
+
+	while(current <= finish) {
+
+		yield current.toISOString().substring(0,10);
+		current = new Date(current.getTime() + 864E5 *step)
+	}
+}
+
