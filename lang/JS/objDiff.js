e5604b8e23bfe7be5983f0f5feb4a30421dde36d
lngd <lngdeer@gmail.com>
2025-08-02 09:15:15 +0300

    lang/JS/objDiff

---
+++ lang/JS/objDiff.js
@@ -0,0 +1,31 @@
+
+
+// LC2700
+// Write a function that accepts two deeply nested objects or arrays obj1 and obj2
+// and returns a new object representing their differences.
+// The function should compare the properties of the two objects and identify any changes.
+// The returned object should only contains keys where the value is different from obj1 to obj2.
+// For each changed key, the value should be represented as an array [obj1 value, obj2 value].
+// Keys that exist in one object but not in the other should not be included in the returned object.
+// The end result should be a deeply nested object where each leaf value is a difference array.
+// When comparing two arrays, the indices of the arrays are considered to be their keys. 
+// You may assume that both objects are the output of JSON.parse.
+
+
+function objDiff(obj1, obj2) {
+
+	if(obj1 === obj2) return {};
+	if(!obj1 || !obj2) return [ obj1,obj2 ];
+	if(Array.isArray(obj1) !== Array.isArray(obj2)) return [ obj1,obj2 ];
+	if(typeof(obj1) !== "object" || typeof(obj2) !== "object") return [ obj1,obj2 ];
+
+	let newObj = {};
+	let current;
+	let key;
+
+	for(key in obj1)
+		if(key in obj2 && (current = objDiff(obj1[key], obj2[key])) && 0 <Object.keys(current).length)
+			newObj[key] = current;
+	return	newObj
+}
+
