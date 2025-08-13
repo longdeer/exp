345249ce13d500494602e35ce2bcf50bedf8208d
lngd <lngdeer@gmail.com>
2025-08-13 15:41:37 +0300

    math/bm/rightmost_bit.py (isolation and bit_count)

--- math/bm/rightmost_bit.py
+++ math/bm/rightmost_bit.py
@@ -1,4 +1,5 @@
 
 
-def rightmost_bit(x :int) -> int : return int(math.log2(-x &x)) +1
+def rightmost_bit1(x :int) -> int : return int(math.log2(-x &x)) +1
+def rightmost_bit2(x :int) -> int : return (-x &x).bit_length()
 
