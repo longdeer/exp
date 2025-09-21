e16f5f22db3a6bf5b19f817ffebcd59147607148
lngd <lngdeer@gmail.com>
2025-09-21 16:59:08 +0300

    lang/JS/addStyles

---
+++ lang/JS/addStyles.js
@@ -0,0 +1,29 @@
+
+
+function addStyles(styles) {
+
+	let styleElement;
+	let styleSheet;
+
+	if(document.createStyleSheet) styleSheet = document.createStyleSheet();
+	else {
+
+		let head = document.getElementsByName("head")[0];
+		styleElement = document.createElement("style");
+		head.appendChild(styleElement);
+		styleSheet = document.styleSheets[document.styleSheets.length-1]
+	}
+	if(typeof(styles) === "string") {
+
+		if(styleElement) styleElement.innerHTML = styles;
+		else styleSheet.cssText = styles					// IE API
+	}	else {
+
+		let i = 0;
+
+		for(selector in styles)
+			if(styleSheet.insertRule) styleSheet.insertRule(`${selector}{${styles[selector]}}`,i++);
+			else styleSheet.addRule(selector, styles[selector], i++)
+	}
+}
+
