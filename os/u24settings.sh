d57cd1f1c72e4758512002be731b10d9836387a7
lngd <lngdeer@gmail.com>
2025-11-03 22:31:51 +0300

    os/u24settings update

--- os/u24settings.sh
+++ os/u24settings.sh
@@ -4,14 +4,7 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
 echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 sudo apt update && sudo apt upgrade -y
-sudo apt install sublime-text sublime-merge
-sudo apt install gnome-shell-extension-manager
-sudo apt install gnome-tweaks
-sudo apt install git
-sudo apt install extrepo
+sudo apt install sublime-text sublime-merge gnome-shell-extension-manager gnome-tweaks git extrepo cifs-utils net-tools vlc gimp python3-venv virtualbox virtualbox-guest-additions-iso
 sudo extrepo enable librewolf
 sudo apt install librewolf
 sudo snap install brave
-sudo apt install cifs-utils net-tools
-sudo apt install vlc
-sudo apt install python3-venv
