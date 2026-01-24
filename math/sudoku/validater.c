ed142c6a6ab73a8809ff0a3db6cd9d1daeda8536
lngd <lngdeer@gmail.com>
2026-01-24 23:06:43 +0300

    math/sudoku/validater.c (bytearray optimized)

--- math/sudoku/validater.c
+++ math/sudoku/validater.c
@@ -10,27 +10,20 @@
 //  Only the filled cells need to be validated according to the mentioned rules.
 
 
-
 bool isValidSudoku(char** board, int boardSize, int* boardColSize)
 {
-	int** rows = malloc(sizeof(int*) *9);
-	int** cols = malloc(sizeof(int*) *9);
-	int** boxs = malloc(sizeof(int*) *9);
+	int* rows = calloc(9,sizeof(int));
+	int* cols = calloc(9,sizeof(int));
+	int* boxs = calloc(9,sizeof(int));
 
 	int currentSymbol;
 	int invalid = 0;
 	int stop = 0;
-	int index;
 	int box;
 	int x;
 	int y;
+	int i;
 
-	for(x = 0; x <9; ++x)
-	{
-		*(rows +x) = calloc(9,sizeof(int));
-		*(cols +x) = calloc(9,sizeof(int));
-		*(boxs +x) = calloc(9,sizeof(int));
-	}
 	for(x = 0; x <9; ++x)
 	{
 		for(y = 0; y <9; ++y)
@@ -39,40 +32,31 @@ bool isValidSudoku(char** board, int boardSize, int* boardColSize)
 
 			if(currentSymbol != '.')
 			{
-				index = currentSymbol - '1';
+				i = 1 <<(currentSymbol - '1');
 
-				if(*(*(rows +x) +index))
+				if(*(rows +x) &i)
 				{
 					invalid = stop = 1;
 					break;
 				}
-				if(*(*(cols +y) +index))
+				if(*(cols +y) &i)
 				{
 					invalid = stop = 1;
 					break;
 				}
 				box = (x /3) *3 + (y /3);
 
-				if(*(*(boxs +box) +index))
+				if(*(boxs +box) &i)
 				{
 					invalid = stop = 1;
 					break;
 				}
-				*(*(rows +x) +index) = 1;
-				*(*(cols +y) +index) = 1;
-				*(*(boxs +box) +index) = 1;
+				*(rows +x) ^= i;
+				*(cols +y) ^= i;
+				*(boxs +box) ^= i;
 			}
 		}
 		if(stop) break;
-	}
-	for(x = 0; x <9; ++x)
-	{
-		free(*(rows +x));
-		free(*(cols +x));
-		free(*(boxs +x));
-		*(rows +x) = NULL;
-		*(cols +x) = NULL;
-		*(boxs +x) = NULL;
 	}
 	free(rows);
 	free(cols);
